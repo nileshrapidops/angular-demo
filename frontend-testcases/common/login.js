5a3ec0d9c8240582cfe7c6ec82e9aca673a54aef
nilesh <nilesh.mistry@rapidops.com>
2015-10-22 12:58:17 +0530

    changed test style from callback to q

--- frontend-testcases/common/login.js
+++ frontend-testcases/common/login.js
@@ -1,29 +1,32 @@
 /*
 	Author: Nilesh Mistry
 */
-
 /*
 	This function is used for login
 	callback : function will execute after complition of login
 */
-module.exports.login = function(username, password, callback){
+module.exports.login = function(username, password){
 	if(username == null || username == undefined)
 		username = "";
 	if(password == null || password == undefined)
 		password = "";
+	return client.setValue('#email', username)
+		.setValue('#password', password)
+		.click('#loginBtn');
+
 	// enter username
-	client.setValue('#email', username, function(err){
-		errorScreenShot(err, 'Entering user email error should be null');
-		// enter password
-		client.setValue('#password', password, function(err){
-			errorScreenShot(err, 'Entering password error should be null');
-			// click on login button
-			client.click('#loginBtn', function(err){
-				errorScreenShot(err, 'Clicking on login button error should be null');
-				client.pause(1000, function(err){
-					callback();
-				});
-			});
-		});
-	});
+	// client.setValue('#email', username, function(err){
+	// 	errorScreenShot(err, 'Entering user email error should be null');
+	// 	// enter password
+	// 	client.setValue('#password', password, function(err){
+	// 		errorScreenShot(err, 'Entering password error should be null');
+	// 		// click on login button
+	// 		client.click('#loginBtn', function(err){
+	// 			errorScreenShot(err, 'Clicking on login button error should be null');
+	// 			client.pause(1000, function(err){
+	// 				callback();
+	// 			});
+	// 		});
+	// 	});
+	// });
 };
--- frontend-testcases/login/login.spec.js
+++ frontend-testcases/login/login.spec.js
@@ -3,78 +3,59 @@
 */
 
 describe('Login Logout Test', function(){
-	this.timeout(999999999);
+	// this.timeout(999999999);
 	before(function(done) {
-		// go to login
-		client.url(host, function(err){
-			errorScreenShot(err, 'Entering url error should be null');
-			// wait for login button
-			client.waitForVisible('#loginBtn', 50000, function(err){
-				errorScreenShot(err, 'Loading login button error should be null');
-				done();
-			});
-		});
+		client.url(host)
+		.waitForVisible('#loginBtn', 50000)
+		.call(done);
 	});
 
 	it('Should Show Error Message If User Email Is Blank', function(done){
-		// login
-		Login.login(null, frontTestConfig.automatedTest.auth.password, function(){
-			client.getText('//*[@id="emailError"]/span[1]', function(err, text){
-				assert.equal('Please provide user email.', text, 'Error message should be "Please provide user email."');
-				done();
-			});
-		});
+		return Login.login(null, frontTestConfig.automatedTest.auth.password)
+		.getText('//*[@id="emailError"]/span[1]', function(err, text){
+			assert.equal('Please provide user email.', text, 'Error message should be "Please provide user email."');
+		})
+		.call(done);
 	});
 
 	it('Should Show Error Message If Password Is Blank', function(done){
-		// login
-		Login.login(frontTestConfig.automatedTest.auth.email, null, function(){
-			client.getText('//*[@id="passwordError"]/span[1]', function(err, text){
-				assert.equal('Please provide password.', text, 'Error message should be "Please provide password."');
-				done();
-			});
-		});
+		return Login.login(frontTestConfig.automatedTest.auth.email, null)
+		.getText('//*[@id="passwordError"]/span[1]', function(err, text){
+			assert.equal('Please provide password.', text, 'Error message should be "Please provide password."');
+		})
+		.call(done);
 	});
 
 	it('Should Show Error Message If Email Is Invalid', function(done){
-		// login
-		Login.login('abc', frontTestConfig.automatedTest.auth.password, function(){
-			client.getText('//*[@id="emailError"]/span[2]', function(err, text){
-				assert.equal('Please provide valid user email.', text, 'Error message should be "Please provide valid user email."');
-				done();
-			});
-		});
+		return Login.login('abc', frontTestConfig.automatedTest.auth.password)
+		.getText('//*[@id="emailError"]/span[2]', function(err, text){
+			assert.equal('Please provide valid user email.', text, 'Error message should be "Please provide valid user email."');
+		})
+		.call(done);
 	});
 
 	it('Should Show Error Message If Email Is Incorrect', function(done){
-		// login
-		Login.login('nilesh.mistry@rapidops.com', frontTestConfig.automatedTest.auth.password, function(){
-			client.getText('#invalidUserError', function(err, text){
-				assert.equal('Invalid User', text, 'Error message should be "Invalid User"');
-				done();
-			});
-		});
+		return Login.login('nilesh.mistry@rapidops.com', frontTestConfig.automatedTest.auth.password)
+		.getText('#invalidUserError', function(err, text){
+			assert.equal('Invalid User', text, 'Error message should be "Invalid User"');
+		})
+		.call(done);
 	});
 
 	it('Should Show Error Message If Password Is Incorrect', function(done){
-		// login
-		Login.login(frontTestConfig.automatedTest.auth.email, '123', function(){
-			client.getText('#invalidUserError', function(err, text){
-				assert.equal('Invalid User', text, 'Error message should be "Invalid User"');
-				done();
-			});
-		});
+		return Login.login(frontTestConfig.automatedTest.auth.email, '123')
+		.getText('#invalidUserError', function(err, text){
+			assert.equal('Invalid User', text, 'Error message should be "Invalid User"');
+		})
+		.call(done);
 	});
 
 	it('Should Move To Products List Page With Proper Authentication', function(done){
-		// login
-		Login.login(frontTestConfig.automatedTest.auth.email, frontTestConfig.automatedTest.auth.password, function(){
-			client.waitForVisible('#productList', 30000, function(err){
-				client.url(function(err, url){
-					assert.equal(host+'/#/app/products/list', url.value, 'Should move to "'+host+'/#/app/products/list"');
-					done();
-				});
-			});
-		});
+		return Login.login(frontTestConfig.automatedTest.auth.email, frontTestConfig.automatedTest.auth.password)
+		.waitForVisible('#productList')
+		.url(function(err, url){
+			assert.equal(host+'/#/app/products/list', url.value, 'Should move to "'+host+'/#/app/products/list"');
+		})
+		.call(done);
 	});
 });
