2efad8bc122b93f6f82cad3f1e0a456c8d61f72c
nilesh <nilesh.mistry@rapidops.com>
2015-10-23 10:46:40 +0530

    added invalid id

--- frontend-testcases/product/product-list.spec.js
+++ frontend-testcases/product/product-list.spec.js
@@ -5,7 +5,8 @@
 describe('Product List Test', function(){
 	// this.timeout(999999999);
 	it('Should Test Filter', function(done){
-		client.moveToObject('//*[@id="center"]/div/div[1]/div[2]/div/div[1]/span')
+		// client.moveToObject('//*[@id="center"]/div/div[1]/div[2]/div/div[1]/span')
+		client.moveToObject('#nilesh')
 		.click('//*[@id="center"]/div/div[1]/div[2]/div/div[1]/span')
 		.setValue('//*[@id="borderLayout_eRootPanel"]/div[3]/span/div/div[2]/input', 'monaco')
 		.click('//*[@id="borderLayout_eRootPanel"]/div[3]/span/div/button[1]')
